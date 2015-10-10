8de2ee2db0101b8274d04d48e850ea1d34c8584a
horaceheaven <hheaven@medullan.com>
2015-10-10 11:29:04 -0400

    created template cache

--- main/main.go
+++ main/main.go
@@ -2,32 +2,43 @@ package main
 
 import (
 	"net/http"
+	"os"
 	"text/template"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		w.Header().Add("Content-Type", "text/html")
-		tmpl, err := template.New("test").Parse(doc)
-		if err == nil {
-			context := Context{"context message"}
-			tmpl.Execute(w, context)
-		}
-	})
+	templates := populateTemplates()
+	http.HandleFunc("/",
+		func(w http.ResponseWriter, req *http.Request) {
+			requestFile := req.URL.Path[1:]
+			template := templates.Lookup(requestFile + ".html")
+			if template != nil {
+				template.Execute(w, nil)
+			} else {
+				w.WriteHeader(http.StatusNotFound)
+			}
+		})
 
 	http.ListenAndServe(":8000", nil)
 }
 
-const doc = `
-<!DOCTYPE html>
-<html>
-<head><title>Go App</title></head>
-<body>
-	<h1>Go Web App Loc: {{.Message}}</h1>
-</body>
-</html>
-`
+func populateTemplates() *template.Template {
+	result := template.New("templates")
 
-type Context struct {
-	Message string
+	basePath := "templates"
+	templateFolder, _ := os.Open(basePath)
+	defer templateFolder.Close()
+
+	templatePathsRaw, _ := templateFolder.Readdir(-1)
+
+	templatePaths := new([]string)
+	for _, pathInfo := range templatePathsRaw {
+		if !pathInfo.IsDir() {
+			*templatePaths = append(*templatePaths,
+				basePath + "/" + pathInfo.Name())
+		}
+	}
+
+	result.ParseFiles(*templatePaths...)
+	return result
 }
